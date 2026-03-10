06f680dee8c4afc92f8743472cc6071103a32b79
Premnath <premnathb77@gmail.com>
2026-03-11 05:10:40 +0530

    actions

--- main.tf
+++ main.tf
@@ -11,17 +11,4 @@ resource "aws_internet_gateway" "main" {
   tags = local.igw_final_tags
 }
 
-#public subnets
-
-resource "aws_subnet" "public" {
-  count = length(var.public_subnet_cidrs)  
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidrs[count.index]
-
-  tags = {
-    Name = "Main"
-  }
-
-  
-}
 
