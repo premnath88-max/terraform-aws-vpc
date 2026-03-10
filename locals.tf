c8347927e23e14ee025c6462d458ee9cff2204ef
Premnath <premnathb77@gmail.com>
2026-03-11 04:41:19 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -14,4 +14,12 @@ vpc_final_tags = merge(
     },
     var.vpc_tags
   )
+
+  igw_final_tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    },
+    var.igw_tags
+  )
 }
