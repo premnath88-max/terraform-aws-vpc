62da7090b34ab6d198386bde44ed199f2b01ddb3
Premnath <premnathb77@gmail.com>
2026-03-13 18:25:25 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -19,6 +19,15 @@ locals {
             },
         var.igw_tags
         )
+
+        az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+        public_subnet_final_tags = merge (
+            local.common_tags,
+            var.public_subnet_tags,
+        )
+
+
+
 }
     
     
